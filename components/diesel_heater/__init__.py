b916898ee95271cc067c5c6846ff9bdddb4fdd89
MakerMylo <maker@mylo.fun>
2025-05-17 09:50:11 +0100

    Most basic and able to compile

--- components/diesel_heater/__init__.py
+++ components/diesel_heater/__init__.py
@@ -1,2 +1,14 @@
-# components/diesel_heater/__init__.py
-# This file can be empty
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID
+from esphome import automation
+import esphome.codegen as cg
+
+# Define the namespace and class
+diesel_heater_ns = cg.esphome_ns.namespace('diesel_heater')
+DieselHeater = diesel_heater_ns.class_('DieselHeater', cg.Component)
+
+# Define the configuration schema
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DieselHeater),
+}).extend(sensor.SENSOR_SCHEMA)
